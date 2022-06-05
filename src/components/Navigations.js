08d3764f13166712b418ec110d61b3c8e71187cc
fpatrilla <fpatrilla@gmail.com>
2022-06-05 19:19:29 -0300

    clase2.1

--- src/components/Navigations.js
+++ src/components/Navigations.js
@@ -3,21 +3,24 @@ import React, { Component } from "react";
 class Navigation extends Component {
     render(){
         return(
+          <nav>
+              <div className="container">
+
+                  <div className="row justify-content-md-center">
+                    <div className="col col-lg-2">
+                    <a href="#inicio" class="nav-item nav-link active">Inicio</a>
+                    </div>
+                    <div className="col-md-auto">
+                    <a href="#laempresa" className="nav-item nav-link">¿Quiénes somos?</a>
+                    </div>
+                    <div class="col col-lg-2">
+                    <a href="#carrito" className="nav-item nav-link">Carrito</a>
+                    </div>
+                  </div>
+
         
-<div className="container">
-  <div className="row justify-content-md-center">
-    <div className="col col-lg-2">
-    <a href="#inicio" class="nav-item nav-link active">Inicio</a>
-    </div>
-    <div className="col-md-auto">
-    <a href="#laempresa" className="nav-item nav-link">¿Quiénes somos?</a>
-    </div>
-    <div class="col col-lg-2">
-    <a href="#carrito" className="nav-item nav-link">Carrito</a>
-    </div>
-  </div>
-  
-</div>
+               </div>
+           </nav>
 
                 
           
